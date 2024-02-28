fad88e90866a605a49bfbe1d77ae36370ea5e507
EzequielEstima <ezequielestima@outlook.pt>
2024-02-28 14:56:50 +0000

    Added repo call to TicketService

--- backend/Services/ImplServices/ProductService.cs
+++ backend/Services/ImplServices/ProductService.cs
@@ -10,43 +10,31 @@ public class ProductService : IProductService
 {
     private IProductRepo productRepo;
 
-    static List<Product> products = new List<Product> {
-        new Product { ProductId = 1, Nome = "Produto 1" },
-        new Product { ProductId = 2, Nome = "Produto 2" },
-        new Product { ProductId = 3, Nome = "Produto 3" },
-        new Product { ProductId = 4, Nome = "Produto 4" },
-        new Product { ProductId = 5, Nome = "Produto 5" }
-    };
-
-    static int nextId = 6;
-
     public ProductService(IProductRepo productRepo)
     {
         this.productRepo = productRepo;
     }
 
-    public Task<ProductDTO> CreateProduct(CreateProductDTO newProduct)
+    public async Task<ProductDTO> CreateProduct(CreateProductDTO newProduct)
     {
-        var product = new Product { ProductId = nextId++, Nome = newProduct.Nome };
+        var product = await productRepo.createProduct(new Product {Nome = newProduct.Nome }); // ID is autoincremented by DB, so we don't need to set it here
 
-        products.Add(product); //TODO call repo 
-        
-        return Task.FromResult(ProductMapper.ToProductDTO(product));
+        return ProductMapper.ToProductDTO(product);
     }
 
-    public Task<ProductDTO?> GetProductById(int id)
+    public async Task<ProductDTO?> GetProductById(int id)
     {
-        var product = products.FirstOrDefault(p => p.ProductId == id);// TODO call repo
+        var product = await productRepo.getProductById(id);
 
         if(product == null) {
-            return Task.FromResult<ProductDTO?>(null);
+            return null;
         }
 
-        return Task.FromResult<ProductDTO?>(ProductMapper.ToProductDTO(product));
+        return ProductMapper.ToProductDTO(product);
     }
 
-    public Task<ListProductDTO> GetProducts()
+    public async Task<ListProductDTO> GetProducts()
     {
-        return Task.FromResult(ProductMapper.ToListProductDTO(products));// TODO call repo
+        return ProductMapper.ToListProductDTO(await productRepo.getProducts());
     }
 }
